8783922457c154decf9f689477fddaa714085504
William Peltomaki <william.peltomaki@trustly.com>
2019-09-18 09:54:15 +0200

    Implement session cache to store session data
    With this commit we can now store session data in a cache which
    is added to the body when requesting the server using a uuid that
    has session data stored in the cache.

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ import nodeCache from 'node-cache';
 const PORT = 5000;
 const uuidv4 = require('uuid/v4');
 const cache = new nodeCache({ stdTTL: 30, checkperiod: 120 });
+const sessionCache = new nodeCache({ stdTTL: 500, checkperiod: 120 });
 const app = express();
 app.use(bodyParser.json())
 
 app.post('/cypress/v1/run/:specName', (req, res) => {
 
-  req.body.uuid = uuidv4();
+  if (req.body.uuid == undefined) {
+	req.body.uuid = uuidv4();
+  } else {
+	req.body.session = sessionCache.get(req.body.uuid);
+  }
+  
   req.body.port = PORT;
   console.log('Requesting spec ' + req.params.specName + ' with configuration:');
   console.log(req.body);
@@ -21,8 +27,10 @@ app.post('/cypress/v1/run/:specName', (req, res) => {
       env: req.body
     })
     .then(results => {
+      const responseValues = cache.get(req.body.uuid);
+      responseValues.uuid = req.body.uuid;
       res.status(200).send({
-      values: cache.get(req.body.uuid),
+      values: responseValues,
       status: results.totalFailed,
       result: results
       })
@@ -37,6 +45,25 @@ app.post('/cache/:id', (req, res) => {
   console.log('Caching values with ID ' + req.params.id);
   console.log(req.body);
   cache.set(req.params.id, req.body, function(error, success){
+    if (!error && success) {
+      res.status(200).send({
+        id: req.params.id,
+        cache: cache.get(req.params.id)
+      });
+    } else {
+      res.status(500).send({
+        id: req.params.id
+      });
+    }
+  });
+
+});
+
+app.post('/session-cache/:id', (req, res) => {
+
+  console.log('Caching session values with ID ' + req.params.id);
+  console.log(req.body);
+  sessionCache.set(req.params.id, req.body, function(error, success){
     if (!error && success) {
       res.status(200).send({
         id: req.params.id,
