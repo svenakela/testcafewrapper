b0a0573208571484dac7158b4db68a4d164aa6c7
Sven Wesley <sven.wesley@trustly.com>
2019-09-18 11:07:49 +0200

    Cache changes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,16 +9,12 @@
 // ***********************************************************
 
 const http = require('http');
-const type = { 
-  CACHE: 'cache',
-  SESSION: 'session'
-}
 
 const cached = (cache, value, config) => {
   let options = {
     url: 'http://localhost',
     port: config.env.port,
-    path: '/cache/v1/' + config.env.uuid,
+    path: '/cache/v1/' + cache + '/' + config.env.uuid,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -34,11 +30,11 @@ module.exports = (on, config) => {
   on('task', {
     cache(value) {
       value.uuid = config.env.uuid;
-      return cached(type.CACHE, value, config);
+      return cached('cache', value, config);
     },
 
     storeSession(value) {
-      return cached(type.SESSION, value, config);
+      return cached('session', value, config);
     }
   })
 }
